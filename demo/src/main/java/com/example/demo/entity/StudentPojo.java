9b53a18ab42dd54d63302d9cf1714bc54e023c9c
vijayhanumanthu <vijayhanumanthums@gmail.com>
2025-10-15 10:34:13 -0500

    adding Pojo class

---
+++ demo/src/main/java/com/example/demo/entity/StudentPojo.java
@@ -0,0 +1,51 @@
+package com.example.demo.entity;
+
+public class StudentPojo {
+
+	private int studentId;
+	private String studentName;
+	private String studentClass;
+	private String studentSection;
+	private String studentBloodGroup;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentClass() {
+		return studentClass;
+	}
+
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+
+	public String getStudentSection() {
+		return studentSection;
+	}
+
+	public void setStudentSection(String studentSection) {
+		this.studentSection = studentSection;
+	}
+
+	public String getStudentBloodGroup() {
+		return studentBloodGroup;
+	}
+
+	public void setStudentBloodGroup(String studentBloodGroup) {
+		this.studentBloodGroup = studentBloodGroup;
+	}
+
+}
